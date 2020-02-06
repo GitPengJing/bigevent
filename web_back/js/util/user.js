724f3ad4956625b992bdef7f17d1ec804e122748
PengBillion <954483281@qq.com>
2020-02-06 21:40:36 +0800

    用户登录功能完成

--- web_back/js/util/user.js
+++ web_back/js/util/user.js
@@ -1,1 +1,9 @@
-var user = {}
+var user = {
+    // 用户登录
+    login: function (name, password) {
+        return $.post(APILIST.user_login, {
+            'user_name': name,
+            'password': password
+        })
+    },
+}
