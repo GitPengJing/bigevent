97ec834a6651d3b62394b5e85fb0ef2d9cd6baaa
PengBillion <954483281@qq.com>
2020-02-06 21:59:59 +0800

    用户退出和获取用户信息功能完成

--- web_back/js/util/config.js
+++ web_back/js/util/config.js
@@ -8,4 +8,6 @@ var APILIST = {
     user_login: BASEURL + '/admin/login',
     // 用户退出
     user_logout: BASEURL + '/admin/logout',
+    // 获取用户信息
+    user_getuser: BASEURL + '/admin/getuser',
 }
--- web_back/js/util/user.js
+++ web_back/js/util/user.js
@@ -6,4 +6,12 @@ var user = {
             'password': password
         })
     },
+    // 用户退出
+    logout: function () {
+        return $.post(APILIST.user_logout)
+    },
+    // 获取用户信息
+    getuser: function () { 
+        return $.get(APILIST.user_getuser)
+     }
 }
