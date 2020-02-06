cab4df96ed18a2985dfec742889aa9cddb648d67
PengBillion <954483281@qq.com>
2020-02-06 21:22:59 +0800

    模块化开发

---
+++ web_back/js/util/config.js
@@ -0,0 +1,11 @@
+// 接口地址
+// 基地址
+var BASEURL = 'http://localhost:8000';
+
+// 接口列表
+var APILIST = {
+    // 用户登录
+    user_login: BASEURL + '/admin/login',
+    // 用户退出
+    user_logout: BASEURL + '/admin/logout',
+}
