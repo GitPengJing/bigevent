e58f552bad87df654250fded01e8eedc0551ccd7
PengBillion <954483281@qq.com>
2020-02-08 23:32:10 +0800

    文章显示完成

--- web_back/js/util/article.js
+++ web_back/js/util/article.js
@@ -1,7 +1,12 @@
 // 文章管理
 var article = {
     // 获取文章
-    get: function () { 
-        
-     }
+    get: function (state, page, id, type) {
+        return $.get(APILIST.article_get, {
+            'state': state,
+            'page': page,
+            'id': id,
+            'type': type
+        })
+    }
 }
